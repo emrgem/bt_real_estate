9d31d271c427b8c5d1794b5b46c1b6a63573341a
emrgem <emrgem@bergen.org>
2025-03-03 09:54:40 -0500

    localhost added

--- btre/settings.py
+++ btre/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['btrealestate-production-384e.up.railway.app']
+ALLOWED_HOSTS = ['btrealestate-production-384e.up.railway.app','localhost', '127.0.0.1']
 
 
 # Application definition
